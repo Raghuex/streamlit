2869d3bf437a78afc1abf429f2baed6313de54ab
Raghuex <raghuvir.iitg@gmail.com>
2024-08-29 17:09:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,15 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
-st.title('Machine Learning App')
+st.title('🤖 Machine Learning App')
 
-st.info('This is a machine learning app')
+st.info('This is app builds a machine learning model!')
 
 with st.expander('Data'):
   st.write('**Raw data**')
-  df = pd.read_csv("penguins_cleaned.csv")
+  df = pd.read_csv('penguins_cleaned.csv')
   df
 
   st.write('**X**')
@@ -21,29 +23,25 @@ with st.expander('Data'):
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
-# SideBar
+# Input features
 with st.sidebar:
   st.header('Input features')
   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
   gender = st.selectbox('Gender', ('male', 'female'))
-  #creating slider for numeric data using their min-max
-  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6)
-  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5)
-  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0)
-  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0)
-
-# create a dataframe for input features
-data = {
-  'island':island,
-  'bill_length_mm': bill_length_mm,
-  'bill_depth_mm': bill_depth_mm,
-  'flipper_length_mm': flipper_length_mm,
-  'body_mass_g': body_mass_g,
-  'sex': gender
-}
-
-input_df = pd.DataFrame(data, index=[0])
-input_penguins = pd.concat([input_df, X_raw], axis=0)
+  
+  # Create a DataFrame for the input features
+  data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
+  input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
 
 with st.expander('Input features'):
   st.write('**Input penguin**')
@@ -121,5 +119,3 @@ st.dataframe(df_prediction_proba,
 
 penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
 st.success(str(penguins_species[prediction][0]))
-
-  
